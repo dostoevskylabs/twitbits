0cfa52da3f620866815f44421587bee61b8aa6b0
Elijah Seymour <dostoevskylabs@users.noreply.github.com>
2017-08-29 00:17:42 -0400

    Update twitbits.js

--- twitbits.js
+++ twitbits.js
@@ -26,29 +26,26 @@ let users = db.get('users').value();
 Twitter.on('direct_message', function(event){
   let username = event["direct_message"]["sender_screen_name"];
   let userid = event["direct_message"].sender_id_str
-  let raw_message = event["direct_message"]["text"].split(" ");
-  let message = [];
+  let message = event["direct_message"]["text"].split(" ");
   let tags = [];
-  // loop through the user_mentions object array
-  // we have to use this because twitter has a security mechanism in place
-  // that will stop the ability to send tags in direct messages if you send too many
-  for ( let i = 0; i < event["direct_message"]["entities"]["user_mentions"].length; i++ ) {
-    tags.push(event["direct_message"]["entities"]["user_mentions"][i].screen_name);
+  // loop through the hashtags object array
+  for ( let i = 0; i < event["direct_message"]["entities"]["hashtags"].length; i++ ) {
+    tags.push(event["direct_message"]["entities"]["hashtags"][i].text);
   }  
   // User is not in our ACL, gtfo
   if ( !users.includes(username) ) return false;
+  let cmd = message[0];
   // Remove the command and the tags (which are actually mentions) from the message we will store
-  raw_message.forEach(function(word){
-   if ( word.indexOf(".") === -1 && word.indexOf("@") === -1 ) message.push(word);
+  message.filter(function(word){
+   if ( word.indexOf(".") === -1 && word.indexOf("#") === -1 ) return true;
   });
-  let cmd = raw_message[0];
   // Command Controller
   const commands = {
     /**
      * Help
      */
     ".h":function(){
-      sendDM(T, userid, `[bot] usage: .a #tags example\n1) .a @tag1 @tag2 text - add tidbit\n2) .t - list tags\n3) .s @tag1 @tag2 - search tidbits for tags\n4) .d id - delete tidbit by id`);
+      sendDM(T, userid, `[info] a list of commands you can use\nthe database is specific to you and no one can see what you save\n1) .a #tag1 #tag2 text - add tidbit\n2) .t - list tags\n3) .s #tag1 #tag2 - search tidbits for tags\n4) .d id - delete tidbit by id`);
     },
     /**
      * Tags
@@ -58,17 +55,15 @@ Twitter.on('direct_message', function(event){
       let entries = db.get('entries')
                   .filter({ username: username })
                   .value();
-      let composedTags = "";
+      let localizedTags = [];
       entries.forEach(function(entry){
         entry["tags"].forEach(function(tag){
           // only display unique tags
-          if ( composedTags.indexOf(tag) === -1 ) {
-            composedTags += `@${tag} `
-          }
+          if ( localizedTags.indexOf(tag) === -1 ) localizedTags.push(`#${tag}`);
         });
       });
-      if ( composedTags !== "" ) {
-        sendDM(T, userid, `[bot] Tags: ${composedTags}`);
+      if ( localizedTags.length > 0 ) {
+        sendDM(T, userid, `[bot] Tags: ${localizedTags.join(", ")}`);
       }
     },
     /**
@@ -86,7 +81,7 @@ Twitter.on('direct_message', function(event){
           username:username,
           message:message.join(" "),
           tags:tags,
-          link: url[0]
+          link: url[0] // temporarily
         })
         .write();
       sendDM(T, userid, `[bot] Got it, ${username}`);
@@ -102,15 +97,15 @@ Twitter.on('direct_message', function(event){
       if ( result.length === 0 ) { // no results!
         sendDM(T, userid, "[bot] No twitbits found! Try adding some.");
       } else {
-        let composedMessage = "";
+        let localizedMessage = [];
         let i = 1;
         result.forEach(function(entry){
           let link = ( entry.link != undefined ? ` ${entry.link}` : "");
           if ( entry.message === "" && link === "" ) return false;
-          composedMessage += `${i}) ${entry.message}${link}\n`;
+          localizedMessage.push(`${i}) ${entry.message}${link}`);
           i++;
         });
-        sendDM(T, userid, `[bot] twitbits found:\n${composedMessage}`);       
+        sendDM(T, userid, `[bot] twitbits found:\n${localizedMessage.join("\n")}`);       
       }
     }
   };
